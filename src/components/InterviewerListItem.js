1bdcd0a663d9a1a89230f36e4c2da14a2cd1e5f8
Imtiaz <imtiazmiah@gmail.com>
2021-05-11 14:12:04 -0400

    Add Interviewer Item List

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classnames from 'classnames/bind';
+
+export default function DayListItem(props) {
+
+  const interviewerClass = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+  return (
+    //   <li className="interviewers__item">
+    //     <img
+    //       className="interviewers__item-image"
+    //       src="https://i.imgur.com/LpaY82x.png"
+    //       alt="Sylvia Palmer"
+    //     />
+    // Sylvia Palmer
+    //   </li>
+
+    <li 
+    className={interviewerClass}
+    onClick = {() => props.setInterviewer(props.name)}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+    { props.selected && props.name }
+    </li>
+  );
+}
