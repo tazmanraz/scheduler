0041d613ac8446f774a782360b6a992282d487f6
Imtiaz <imtiazmiah@gmail.com>
2021-05-11 17:37:58 -0400

    Add Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+export default function Appointment(props) {
+
+  return (
+    <article className="appointment"></article>
+  )
+}
