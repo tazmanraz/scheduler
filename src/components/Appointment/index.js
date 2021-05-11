93960b13be90bff10365632a3c79fe370c8f249c
Imtiaz <imtiazmiah@gmail.com>
2021-05-11 18:25:38 -0400

    Add Show components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,14 +3,13 @@ import './styles.scss';
 
 import Header from "components/Appointment/Header"
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show"
 
 
 export default function Appointment(props) {
 
   return (
     <article className="appointment">
-          <Header time={props.time}/>
-
     </article>
   )
 }
