81903b57478230fc6abf5b49e47d6b98c04cbbd3
Imtiaz <imtiazmiah@gmail.com>
2021-05-11 21:04:25 -0400

    Add states to form

---
+++ src/components/Appointment/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import InterviewerList from "../InterviewerList";
+import Button from "../Button";
+
+export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            value={name}
+            onChange={ (e) => setName(e.target.value)}
+          /*
+            This must be a controlled component
+          */
+          />
+        </form>
+        <InterviewerList
+          interviewers={props.interviewers}
+          interviewer={interviewer}
+          setInterviewer={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(name, interviewer)}>Save</Button>
+        </section>
+      </section>
+    </main>
+  )
+}
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,6 +7,7 @@ import Show from "components/Appointment/Show"
 import Confirm from "components/Appointment/Confirm"
 import Status from "components/Appointment/Status"
 import Error from "components/Appointment/Error"
+//import Form from "components/Appointment/Form"
 
 
 export default function Appointment(props) {
