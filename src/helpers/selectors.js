15a10099f86dd8c4f60fc974b17b9167e8c954c2
Imtiaz <imtiazmiah@gmail.com>
2021-05-13 16:08:09 -0400

    add getInterview selector

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -12,40 +12,19 @@ export function getAppointmentsForDay(state, day) {
   return apptsForDay;
 }
 
+export function getInterview(state, interview) {
 
-// function selectUserByName(state, name) {
-//   const filteredNames = state.users.filter(user => user.name === name);
-//   return filteredNames;
-// }
-//MONDAY
-// console.log src/helpers/selectors.js:33
-// [
-//   { id: 1, time: '12pm', interview: null },
-//   { id: 2, time: '1pm', interview: null },
-//   {
-//     id: 3,
-//     time: '2pm',
-//     interview: { student: 'Archie Cohen', interviewer: 2 }
-//   }
-// ]
-//MONDAY
-// console.log src/helpers/selectors.js:33
-// [
-//   { id: 1, time: '12pm', interview: null },
-//   { id: 2, time: '1pm', interview: null },
-//   {
-//     id: 3,
-//     time: '2pm',
-//     interview: { student: 'Archie Cohen', interviewer: 2 }
-//   }
-// ]
-//TUESDAY
-// console.log src/helpers/selectors.js:33
-// [
-//   { id: 4, time: '3pm', interview: null },
-//   {
-//     id: 5,
-//     time: '4pm',
-//     interview: { student: 'Chad Takahashi', interviewer: 2 }
-//   }
-// ]
+  if (!interview) return null;
+
+  let fullInterview = {};
+
+  for (let objKey in state.interviewers) {
+    if (state.interviewers[objKey].id === interview.interviewer) {
+      fullInterview = {
+        student: interview.student,
+        interviewer: state.interviewers[objKey]
+      }
+    }
+  }
+  return fullInterview;
+}
